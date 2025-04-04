e0d79eded4c76fc3542f44a85e4be3dc6b9bd6f9
Avijit Biswas <avijit4000@gmail.com>
2025-04-04 21:26:07 +0530

    Renamed main.py to application.py

--- application.py
+++ application.py
@@ -53,4 +53,4 @@ async def speak_recognized_text(request: Request):
     speak_text(text)
     return {"message": "Text spoken aloud"}
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("application:app", host="0.0.0.0", port=8000, reload=True)
